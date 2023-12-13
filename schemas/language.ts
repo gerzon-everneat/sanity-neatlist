3eab0893bab4a3b6c1a2e1e3efc339e638c18467
Ahmad Marhaba <ahmad@everneat.co>
2023-12-13 15:55:48 +0200

    init cms

---
+++ schemas/language.ts
@@ -0,0 +1,27 @@
+export default {
+  name: 'language',
+  type: 'document',
+  title: 'Language',
+  fields: [
+    {
+      name: 'id',
+      type: 'string',
+      title: 'Id',
+    },
+    {
+      name: 'location',
+      type: 'string',
+      title: 'Location',
+    },
+    {
+      name: 'en_US',
+      type: 'text',
+      title: 'English - United States',
+    },
+    {
+      name: 'es',
+      type: 'text',
+      title: 'Spanish',
+    },
+  ],
+}
